69680c76563a88cb25b5cbf456c46d59faff83f9
SalaryTheft <@>
2023-03-07 23:31:48 +0900

    Initial commit

---
+++ FoldableMenu/main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menu_btn = document.querySelector("#menu_btn");
+    const menu = document.querySelector("#main_menu");
+    const content = document.querySelector("#main_content");
+
+    menu_btn.addEventListener("click", () => {
+        menu.classList.toggle("menu-folded");
+        content.classList.toggle("menu-folded");
+    });
+
+    document.querySelectorAll(".menu-list > a").forEach((a_tag) => {
+        a_tag.addEventListener("click", (e) => {
+            document.querySelector(".menu-list > a.active").classList.add("tmp");
+            setTimeout(() => {
+                document.querySelector(".menu-list > a.active.tmp").classList.remove("active", "tmp");
+            }, 100);
+            e.target.closest("a").classList.add("active");
+        });
+    });
+});
